7cd88954b6fea7dfaa9c66490d8e1b63ff288a0c
angus <angus5788@gmail.com>
2019-07-25 22:23:49 +0800

    生成用户认证代码

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,7 +13,7 @@ const mix = require('laravel-mix');
 
 
 
-const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
-  .sass('resources/sass/app.scss', 'public/css').version();
+  .sass('resources/sass/app.scss', 'public/css')
+  .version()
